8173a7a339bb7f4f5b27a931186271ab60fc900f
MiguelRequenaR <e_2019100844l@uncp.edu.pe>
2025-07-27 14:07:15 -0500

    Componente de Boton.

--- src/components/CTA.tsx
+++ src/components/CTA.tsx
@@ -1,3 +1,4 @@
+import BotonContacto from "./BotonContacto"
 
 export default function CTA() {
   return (
@@ -22,14 +23,7 @@ export default function CTA() {
             Encuentra tu hogar ideal, contacta a nuestro equipo de profesionales, para poder conocer el hogar de tus sueños.
           </p>
 
-          <a
-            href="/contacto"
-            className="btn btn-lg text-[#383838] hover:text-primary bg-white border border-gray-300 transition-all duration-300 hover:scale-110 items-center gap-2 group rounded-full"
-          >
-            Contáctanos
-            <svg viewBox="0 0 24 24" className="w-5 h-5 transition-transform icon glyph duration-500 group-hover:rotate-[360deg]" xmlns="http://www.w3.org/2000/svg" id="up-right" fill="#000000"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M19,4H9A1,1,0,0,0,9,6h7.59L4.29,18.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L18,7.41V14.9a1,1,0,0,0,2,0V5A1,1,0,0,0,19,4Z" style={{ fill: '#231f20' }}></path></g></svg>
-
-          </a>
+          <BotonContacto />
         </div>
       </div>
     </section>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Link } from '@tanstack/react-router'
 import { useRef, useEffect } from 'react'
+import BotonContacto from './BotonContacto'
 
 export default function Header() {
   const headerRef = useRef<HTMLElement>(null)
@@ -98,10 +99,9 @@ export default function Header() {
               </label>
               <Link
                 to="/contacto"
-                className="btn btn-lg hidden md:flex text-[#383838] hover:text-primary bg-white border border-gray-300 transition-all duration-300 hover:scale-110 items-center gap-2 group"
+                className="hidden md:flex "
               >
-                Contáctanos
-                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" id="up-right" className="icon glyph size-5" fill="#000000"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M19,4H9A1,1,0,0,0,9,6h7.59L4.29,18.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L18,7.41V14.9a1,1,0,0,0,2,0V5A1,1,0,0,0,19,4Z" style={{ fill: '#373839' }}></path></g></svg>
+                <BotonContacto />
               </Link>
             </div>
           </div>
@@ -188,10 +188,9 @@ export default function Header() {
             <li onClick={() => drawerCheckboxRef.current && (drawerCheckboxRef.current.checked = false)}>
               <Link
                 to="/contacto"
-                className="btn btn-lg text-[#383838] hover:text-primary bg-white border border-gray-300 transition-all duration-300 items-center gap-2 group mt-4"
+                className="flex items-center gap-3 text-primary transition-all duration-300 py-4 text-xl"
               >
-                Contáctanos
-                <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" id="up-right" className="icon glyph size-5" fill="#000000"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M19,4H9A1,1,0,0,0,9,6h7.59L4.29,18.29a1,1,0,0,0,0,1.42,1,1,0,0,0,1.42,0L18,7.41V14.9a1,1,0,0,0,2,0V5A1,1,0,0,0,19,4Z" style={{ fill: '#373839' }}></path></g></svg>
+                <BotonContacto />
               </Link>
             </li>
           </ul>
